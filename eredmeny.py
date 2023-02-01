82384bc69f35eccf60390d7222e19868aa004e8b
Martonnvok <115223813+Martonnvok@users.noreply.github.com>
2023-02-01 12:49:12 +0100

    hibás megoldás

--- eredmeny.py
+++ eredmeny.py
@@ -1,7 +1,16 @@
 #egy komment
-def eredmeny():
-    lapok=21
-    if lapok>21:
-        print("Vesztettél")
-    else:
-        print("nyertél")
+jlapok=[1,2,3,4,5]
+glapok=[6,7,8,9,10]
+def osszeg(lap):
+    ossz = 0
+    for i in lap:
+        ossz += i
+
+    return osszeg
+
+def eredmeny(jatekos, gep  ):
+    if osszeg(jatekos) > 21:
+        print("A játékos Vesztett")
+    elif osszeg(gep) > 21:
+        print("A gép vesztett")
+
