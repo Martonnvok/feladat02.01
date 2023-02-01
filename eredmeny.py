14c21522e480f4fced9858f2c6c7acd6cc5ce214
Martonnvok <115223813+Martonnvok@users.noreply.github.com>
2023-02-01 13:23:18 +0100

    teszt

--- eredmeny.py
+++ eredmeny.py
@@ -1,13 +1,12 @@
-#egy komment
 def eredmeny(jatekoslapok: [int], geplapok: [int]):
-    jatekospont: int = szamolas(jatekoslapok)
-    geppont: int = szamolas(geplapok)
-    if jatekospont > 21:
+    jatekosp: int = szamol(jatekoslapok)
+    gepp: int = szamol(geplapok)
+    if jatekosp > 21:
         return "játékos vesztett"
-    elif geppont > 21:
+    elif gepp > 21:
         return "gép vesztett"
 
-def szamolas(lapok)->int:
+def szamol(lapok)->int:
     pontok: int = 0
     i: int = 0
     while i < len(lapok):
@@ -16,14 +15,26 @@ def szamolas(lapok)->int:
     return pontok
 
 def jatekos_vesztett_teszt():
-    jLapok = [1, 2, 3, 4]
-    gLapok = [5, 6]
-
-    print(eredmeny(jLapok, gLapok))
-
-
+    jatekosp = [10, 5, 7]
+    gepp = [2, 7, 9]
+    #teszt
+    kapott = eredmeny(jatekosp, gepp)
+    vart = "játékos vesztett!"
+    if kapott == vart:
+        print("A teszt sikeres!")
+    else:
+        print("A teszt megbukott")
+def gep_vesztett_teszt():
+    jatekospontok = [10, 5, 7]
+    geppontok = [2, 7, 9]
+    #teszt
+    kapott = eredmeny(jatekospontok, geppontok)
+    vart = "gép vesztett!"
+    if kapott == vart:
+        print("A teszt sikeres!")
+    else:
+        print("A teszt megbukott")
 def tesztek():
     jatekos_vesztett_teszt()
-
 
 tesztek()
